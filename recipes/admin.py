596ad4a51fd0f5a8cac3d583bcd620825c7fe6fe
Elmas Dağlı <eedagli@gmail.com>
2019-07-05 21:15:16 +0300

    database was created

---
+++ recipes/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Recipe,Ingredient,Food,VegetarianFood,MeasureTable
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Food)
+admin.site.register(VegetarianFood)
+admin.site.register(MeasureTable)
