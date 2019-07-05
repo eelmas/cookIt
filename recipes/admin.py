56301169bdff9445a2c157c89f998af7fd87f4f3
Elmas Dağlı <eedagli@gmail.com>
2019-07-05 21:26:45 +0300

    settings.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe,Ingredient,Food,VegetarianFood,MeasureTable
+from .models import Recipe, Ingredient, Food, VegetarianFood, MeasureTable
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
